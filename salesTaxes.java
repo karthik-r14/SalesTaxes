4ed4bfeda1a01a1ae1da9b097ba8b1b85fdc1e01
karthikr <karthikr@INkarthikr.local>
2015-08-24 17:35:50 +0530

    Commit-1

---
+++ salesTaxes.java
@@ -0,0 +1,25 @@
+/**
+ * Created by karthikr on 24/08/15.
+ */
+import java.util.Scanner;
+
+public class salesTaxes {
+
+
+
+    public static void main(String []args )
+    {
+      String str;
+      Scanner a=new Scanner(System.in);
+        System.out.println("Input1:");
+
+      str=a.next();
+
+
+      System.out.println(str);
+
+
+
+    }
+
+}
